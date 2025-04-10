1e221f4155f1d42e8995057cb3ac94a100ccc8b5
viktoriiiamaar <vmarushc@nd.edu>
2025-04-09 20:54:29 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def on_button_pressed_a():
+    basic.pause(1000)
     path = [1, 2, 3, 1, 2, 3, 1]
     for i in range(len(path)):
         radio.send_value("step", path[i])
--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 //  Small delay to ensure transmission
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    basic.pause(1000)
     let path = [1, 2, 3, 1, 2, 3, 1]
     for (let i = 0; i < path.length; i++) {
         radio.sendValue("step", path[i])
